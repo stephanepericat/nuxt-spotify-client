49d53dd74f8f3cb2c51923c0a4aedb41d3a77e21
Stephane P. Pericat <contact@pericat.net>
2021-12-18 13:25:03 -0600

    chore: sass, reset css, layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-container class="spotify-client">
+    <NuxtChild />
+  </el-container>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+<style lang="scss" scoped>
+.spotify-client {
+  // background-color: #111;
+  height: 100vh;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial />
+  <div>
+    <ElButton>spotify</ElButton>
+  </div>
 </template>
 
 <script>
