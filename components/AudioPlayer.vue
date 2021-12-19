d1e0d381f509569bb7df21b142eb8c3bfa1828c1
Stephane P. Pericat <contact@pericat.net>
2021-12-19 13:59:20 -0600

    feat: audio player comp

---
+++ components/AudioPlayer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="audio-player">Audio Player</div>
+</template>
+<script>
+export default {
+  name: 'AudioPlayer',
+}
+</script>
+<style lang="scss" scoped>
+@import '~/assets/style/variables';
+@import '~/assets/style/mixins';
+
+.audio-player {
+  @include centeredPage();
+  height: calc(100% - 10px);
+
+  border-top: 1px solid $color-highlight;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,20 @@
 <template>
   <el-container class="spotify-client default-layout">
-    <el-aside>
+    <el-aside class="spotify-client__sidebar">
       <ul>
         <li v-for="playlist in playlists" :key="playlist.id">
           {{ playlist.name }}
         </li>
       </ul>
     </el-aside>
-    <el-container>
-      <el-header>header</el-header>
-      <el-main>
+    <el-container class="spotify-client__main">
+      <el-header class="spotify-client__header">header</el-header>
+      <el-main class="spotify-client__contents">
         <nuxt-child />
       </el-main>
-      <el-footer>footer</el-footer>
+      <el-footer class="spotify-client__footer" height="100px">
+        <AudioPlayer />
+      </el-footer>
     </el-container>
   </el-container>
 </template>
