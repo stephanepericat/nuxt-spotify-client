da6a924b6ab3556a8f39d4055c8d2105f35f41d1
Stephane P. Pericat <contact@pericat.net>
2021-12-19 18:50:37 -0600

    feat: progress slider

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,36 +1,45 @@
 <template>
   <el-row class="audio-player">
-    <el-button
-      class="audio-player__button"
-      icon="el-icon-sort"
-      type="info"
-      circle
-    ></el-button>
-    <el-button
-      class="audio-player__button"
-      icon="el-icon-d-arrow-left"
-      type="success"
-      circle
-    ></el-button>
-    <el-button
-      class="audio-player__button"
-      :icon="playIcon"
-      type="success"
-      circle
-      @click="togglePlayState"
-    ></el-button>
-    <el-button
-      class="audio-player__button"
-      icon="el-icon-d-arrow-right"
-      type="success"
-      circle
-    ></el-button>
-    <el-button
-      class="audio-player__button"
-      icon="el-icon-refresh"
-      type="info"
-      circle
-    ></el-button>
+    <div class="audio-player__current">info</div>
+    <div class="audio-player__controls">
+      <div class="audio-player__controls--bar">
+        <el-button
+          class="audio-player__button"
+          icon="el-icon-sort"
+          type="info"
+          circle
+        ></el-button>
+        <el-button
+          class="audio-player__button"
+          icon="el-icon-d-arrow-left"
+          type="success"
+          circle
+        ></el-button>
+        <el-button
+          class="audio-player__button"
+          :icon="playIcon"
+          type="success"
+          circle
+          @click="togglePlayState"
+        ></el-button>
+        <el-button
+          class="audio-player__button"
+          icon="el-icon-d-arrow-right"
+          type="success"
+          circle
+        ></el-button>
+        <el-button
+          class="audio-player__button"
+          icon="el-icon-refresh"
+          type="info"
+          circle
+        ></el-button>
+      </div>
+      <div class="audio-player__controls--progress">
+        <el-slider v-model="progress"></el-slider>
+      </div>
+    </div>
+    <div class="audio-player__devices">devices</div>
   </el-row>
 </template>
 <script>
@@ -41,13 +50,14 @@ export default defineComponent({
 
   setup() {
     const isPlaying = ref(false)
+    const progress = ref(0)
     const playIcon = computed(() =>
       isPlaying.value ? 'el-icon-video-pause' : 'el-icon-video-play'
     )
 
     const togglePlayState = () => (isPlaying.value = !isPlaying.value)
 
-    return { playIcon, togglePlayState }
+    return { playIcon, progress, togglePlayState }
   },
 })
 </script>
@@ -63,6 +73,30 @@ export default defineComponent({
     ::v-deep i {
       font-size: 20px;
     }
+  }
+
+  &__current {
+    flex-basis: 300px;
+    height: 60px;
+    // background: #f70;
+  }
+
+  &__controls {
+    flex-grow: 1;
+    flex-shrink: 0;
+    text-align: center;
+    height: 80px;
+    // background: yellow;
+
+    &--progress {
+      padding: 0 20px;
+    }
+  }
+
+  &__devices {
+    flex-basis: 300px;
+    height: 30px;
+    // background: #f70;
   }
 }
 </style>
